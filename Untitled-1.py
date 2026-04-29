816af99947ef78e6dd2aaf98906b6a3b72901d4a
benjaminvaliente-ph <benjamin.valiente@cepbuna.edu.py>
2026-04-29 15:52:39 -0300

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,3 +1,4 @@
+#from paises import paises
 from PyQt5.QtWidgets  import *
 from PyQt5.uic import *
 from PyQt5.QtCore import *
@@ -6,11 +7,10 @@ import sys
 class MiVentana(QMainWindow):
 
     def __init__(self):
-        
+        paises = ["Ecuador", "Brasil", "Argentina", "Armenia", "Mongolia", "Paraguay", "Panama", "Alemania", "Afganistan", "China", "Vietnam"]
         super().__init__()
-        self.paises = ["Ecuador", "Argentina", "Alemania","Paraguay","Panama","Brasil"]
+        self.paises = paises
         loadUi("main.ui", self)
-
         completer = QCompleter(self.paises)
         completer.setCaseSensitivity(Qt.CaseInsensitive)
         self.txtBuscador.setCompleter(completer)
@@ -18,6 +18,12 @@ class MiVentana(QMainWindow):
         self.scrollAreaWidgetContents.setLayout(self.scrollLayout)
         self.txtBuscador.returnPressed.connect(self.Buscar)
         self.btnBuscar.clicked.connect(self.Buscar)
+
+        self.btn1.clicked.connect(lambda _, c=self.Configuraciones_2: self.Expandir(c))
+        self.btn2.clicked.connect(lambda _, c=self.Anotaciones_2: self.Expandir(c))
+        self.btn3.clicked.connect(lambda _, c=self.Cronometro_2: self.Expandir(c))
+        self.btn4.clicked.connect(lambda _, c=self.Historial: self.Expandir(c))
+        self.cerrarSideBar.clicked.connect(lambda: self.sideBar.setMaximumWidth(0))
         
     def Buscar(self):
         item = self.scrollLayout.takeAt(self.scrollLayout.count() - 1)
@@ -36,6 +42,7 @@ class MiVentana(QMainWindow):
     def QuitarPais(self, pais):
         txt = str(pais.text())
         pais.deleteLater()
+        self.txtCronometro.setText(f"03:00 - {txt}")
         self.timer = QTimer()
         self.time = QTime(0, 3, 0) #Tiempo que empieza el cronometro !!
         self.timer.start(1000)
@@ -45,9 +52,18 @@ class MiVentana(QMainWindow):
         self.time = self.time.addSecs(-1)
         timeDisplay = self.time.toString("mm:ss")
         self.txtCronometro.setText(f"{timeDisplay} - {pais}")
-        if self.time == 0:
-             
-
+        if self.time == QTime(0, 0, 0):
+            self.timer.stop()
+            self.txtCronometro.setText(f"00:00")
+    
+    def Expandir(self, nombre):
+        self.sideBar.setMaximumWidth(400)
+        self.Anotaciones_2.setMaximumHeight(0)
+        self.Configuraciones_2.setMaximumHeight(0)
+        self.Historial.setMaximumHeight(0)
+        self.Cronometro_2.setMaximumHeight(0)
+        nombre.setMaximumHeight(400)
+    
 app= QApplication(sys.argv)
 ventana= MiVentana()
 ventana.show()
